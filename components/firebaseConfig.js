0f18ecfdf519516601c72506b55e215f502cb294
Vladimir <48209956+VladimirtheGreatest@users.noreply.github.com>
2019-08-19 22:32:49 +0100

    Add files via upload

---
+++ components/firebaseConfig.js
@@ -0,0 +1,9 @@
+export default{
+  apiKey: "AIzaSyDUtZ5oXjUxhJIC12RLdjtuUclIMUU-CUI",
+    authDomain: "vuefs-prod-ddadd.firebaseapp.com",
+    databaseURL: "https://vuefs-prod-ddadd.firebaseio.com",
+    projectId: "vuefs-prod-ddadd",
+    storageBucket: "vuefs-prod-ddadd.appspot.com",
+    messagingSenderId: "688819392815",
+    appId: "1:688819392815:web:70d4fd220a878b66"
+}
