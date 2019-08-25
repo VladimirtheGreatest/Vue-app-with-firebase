639850581f745c20b2a690588789eea0d8b3e1c5
Vladimir <48209956+VladimirtheGreatest@users.noreply.github.com>
2019-08-25 12:37:37 +0100

    Update main.js
    Preventing user reloading

--- main.js
+++ main.js
@@ -3,13 +3,19 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import firebase from 'firebase';
+import './components/firebaseInit';
 
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
-})
+let app;
+firebase.auth().onAuthStateChanged(function(user) {
+  if (!app) {
+    app = new Vue({
+      el: '#app',
+      router,
+      template: '<App/>',
+      components: { App }
+    });
+  }
+});
